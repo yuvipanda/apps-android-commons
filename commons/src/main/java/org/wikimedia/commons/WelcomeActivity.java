72628b27ff130a9b1645ea34d8a6f2b5e759ea6f
YuviPanda <yuvipanda@gmail.com>
2013-07-27 03:24:05 +0530

    Kill extra import
    Change-Id: Ie324d6999752a149b25bcc34a87cdbffe7dc53ca

--- commons/src/main/java/org/wikimedia/commons/WelcomeActivity.java
+++ commons/src/main/java/org/wikimedia/commons/WelcomeActivity.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-import com.actionbarsherlock.app.SherlockActivity;
 import com.viewpagerindicator.CirclePageIndicator;
 
 public class WelcomeActivity extends Activity {
